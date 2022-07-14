dd0f1fb66f83e5254f8374d4ec8dc2f40e08554c
RoadTLife <roadtlife0111925@163.com>
2022-07-14 22:58:58 +0800

    :tada: complete the pagerank algo

--- src/graph/algo/centrality/PageRank.java
+++ src/graph/algo/centrality/PageRank.java
@@ -4,48 +4,56 @@ import graph.api.core.GraphApi;
 
 import java.util.Arrays;
 
-//TODO: unfinished
+/**
+ * version: 1.0
+ * author: road
+ * data: 20220714
+ */
 public class PageRank {
     private GraphApi graphApi;
-    private double[] initValue;
+    //reduce computational cost
+    private double[] initValues;
     private double[] preValue;
     private double[] curValue;
+    private double delta;
 
     public PageRank(GraphApi graphApi){
         this.graphApi = graphApi;
     }
 
-    public double[] compute(int iteration, double damping) {
-        init(damping);
+    public double[] compute(int iteration, double damping, double initValue) {
+        delta = 1 - damping;
+        init(damping, initValue);
         int vertexSize = graphApi.getVertexSize();
         preValue = new double[vertexSize];
         curValue = new double[vertexSize];
         int[] vertexIds = graphApi.getVertex();
 
         for(int i = 0; i < iteration; i++) {
+            System.arraycopy(curValue, 0, preValue, 0, vertexSize);
             for (int id : vertexIds) {
                 double sum = 0;
                 int[] inEdge = graphApi.getInEdge(id);
                 for (int inId : inEdge) {
-                    sum += initValue[inId] * preValue[inId];
+                    sum += initValues[inId] * preValue[inId];
                 }
-                curValue[id] = sum;
+                //(1-d) + sum
+                curValue[id] = delta +sum;
             }
-            preValue = curValue;
         }
         return curValue;
     }
 
-    private void init(double damping) {
+    private void init(double damping, double initValue) {
         int vertexSize = graphApi.getVertexSize();
-        initValue = new double[vertexSize];
-        Arrays.fill(initValue, damping);
+        initValues = new double[vertexSize];
+        Arrays.fill(curValue, initValue);
 
         int[] vertexIds = graphApi.getVertex();
         for (int id : vertexIds) {
             int outDegree = graphApi.getOutEdge(id).length;
             if(outDegree != 0) {
-                initValue[id] = damping / outDegree;
+                initValues[id] = damping / outDegree;
             }
         }
     }
